f6a289cc50d3460227befd0cd1660b445ec69a11
mashrabyuldashov <yoldashovmashrab@gmail.com>
2022-05-21 11:20:47 -0700

    commited 3

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 
 const mail = require('./utils/mail')
 
 app.use(express.json())
+app.use(cors())
 
 const PORT = process.env.PORT || 9000
 
 app.get('/', async(req, res) => {
     await mail(req.body.email, req.body.password, req.body.subject, req.body.text)
+    console.log(req.body)
     res.end("Baxtiyor")
 })
 
